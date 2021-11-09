a6f0e518471902e14bc2699e9af1da5b6b122a91
Md. Abdul Muhib Sami <saami7264@Mds-MacBook-Air.Dlink>
2021-11-09 20:52:30 +0600

    second

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run() {
 
         app.get('/appointments', async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { email: email, date: date }
 
             const cursor = appointmentsCollection.find(query);
